65c5308a4d69426b3cce4d6bab72fcc1c99e0b1e
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-24 06:10:45 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    """Prints a list of the Fibonacci sequence up to the given length."""
+    if length < 0:
+        raise ValueError("Length must be a positive integer.")
+    if length == 0:
+        print([])
+        return []
+    if length == 1:
+        print([0])
+        return [0]
+    if length == 2:
+        print([0, 1])
+        return [0, 1]
+
+    sequence = [0, 1]
+    for i in range(2, length):
+        sequence.append(sequence[i - 1] + sequence[i - 2])
+    print(sequence)
+    return sequence
